259210b07f64fa33a01b733ae6f21ba44f0b43bc
Joseph Damiba <joseph.damiba@plot.ly>
2020-02-28 00:37:44 -0500

    add multi index search

--- all_static/javascripts/algolia/algolia.js
+++ all_static/javascripts/algolia/algolia.js
@@ -2,6 +2,8 @@
 $(function(config) {
   'use strict';
 
+  
+
   var search = instantsearch({
     // Replace with your own values
     appId: config.applicationId,
@@ -10,7 +12,7 @@ $(function(config) {
     urlSync: false,
     searchFunction: function (helper) {
       if (helper.state.query === '') {
-        document.getElementById('search-results').innerHTML = '';
+        document.getElementById('primary-search-results').innerHTML = '';
         return;
       }
 
@@ -35,21 +37,67 @@ $(function(config) {
 
   search.addWidget(
     instantsearch.widgets.hits({
-      container: '#search-results',
-      hitsPerPage: 10,
+      container: '#primary-search-results',
       templates: {
-        empty: '<div class="text-center">No results found matching <strong>{{query}}</strong>.</div>',
+        empty: '<div class="text-center">No results found matching <strong>{{query}}</strong> Try the search on <a target="_blank" href="https://www.google.com/search?q=site%3Aplot.ly+{{query}}">Google</a>.</div>',
         item: document.getElementById('algolia__template').innerHTML,
       },
       transformData: {
         item: function(hit) {
           hit.raw = JSON.stringify(hit, null, 2);
-          console.log(hit)
           return hit;
+        }
+      }
+    }),
+  );
+
+  var secondarySearch = instantsearch({
+    // Replace with your own values
+    appId: config.applicationId,
+    apiKey: config.apiKey,
+    indexName: 'schema',
+    urlSync: false,
+    searchFunction: function (helper) {
+      if (helper.state.query === '') {
+        document.getElementById('secondary-search-results').innerHTML = '';
+        return;
+      }
+
+      helper.search();
+    }
+  });
+
+  secondarySearch.addWidget(
+    instantsearch.widgets.searchBox({
+      container: "#search-input",
+      magnifier: false,
+      reset: false,
+      queryHook: function(query, search) {
+        if (query === "") {
+          search();
+        } else {
+          search(query);
         }
       }
     })
   );
+
+  secondarySearch.addWidget(
+    instantsearch.widgets.hits({
+      container: '#secondary-search-results',
+      templates: {
+        empty: '<div class="text-center">No results found matching <strong>{{query}}</strong> Try the search on <a target="_blank" href="https://www.google.com/search?q=site%3Aplot.ly+{{query}}">Google</a>.</div>',
+        item: document.getElementById('algolia__secondary-template').innerHTML,
+      },
+      transformData: {
+        item: function(hit) {
+          hit.raw = JSON.stringify(hit, null, 2);
+          return hit;
+        }
+      }
+    }),
+  );
+
 
   document.getElementById("search-results").style.zIndex = -99;
   document.getElementById("search-input").addEventListener("input", function (event){
@@ -60,8 +108,24 @@ $(function(config) {
       document.getElementById("search-results").style.zIndex = 99;
     }
   })
+  document.getElementById("search-input").addEventListener("click", function (event){
+    document.getElementById("search-results").style.zIndex = 99;
+  })
 
+  function hideOnClickOutside(element) {
+    const outsideClickListener = event => {
+        if (!element.contains(event.target) && isVisible(element)) { // or use: event.target.closest(selector) === null
+          element.style.zIndex = -99;
+        }
+    }
+
+    document.addEventListener('click', outsideClickListener)
+  }
+
+  const isVisible = elem => !!elem && !!( elem.offsetWidth || elem.offsetHeight || elem.getClientRects().length ) // source (2018-03-11): https://github.com/jquery/jquery/blob/master/src/css/hiddenVisibleSelectors.js 
+  hideOnClickOutside(document.getElementById("search-results"));
   search.start();
+  secondarySearch.start();
 
 }(window.ALGOLIA_CONFIG));
 
