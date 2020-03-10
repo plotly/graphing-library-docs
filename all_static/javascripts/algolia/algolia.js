7f6c28c312f937bab63996968c1dec4b1603171a
Joseph Damiba <joseph.damiba@plot.ly>
2020-03-10 16:14:27 -0400

    use for loop to instantiate search helpers

--- all_static/javascripts/algolia/algolia.js
+++ all_static/javascripts/algolia/algolia.js
@@ -12,97 +12,59 @@ $(function(config) {
   }
 
   var emptyResult = '<div class="text-center"><br><br>No results found matching <strong>{{query}}</strong>.<br><br> Click here to <a class="algolia__result-link" target="_blank" href="https://www.google.com/search?q=plotly+' + lang + '+{{query}}">search for "plotly ' + lang + ' {{query}}" on Google</a>.</div>';
+
+  var searches = [];
+
+  for (let i = 0; i < 2; i++){
+
+    searches[i] = instantsearch({
+      appId: config.applicationId,
+      apiKey: config.apiKey,
+      indexName: i == 0 ? config.indexName : "schema",
+      urlSync: false,
+      searchFunction: function (helper) {
+        if (helper.state.query === '') {
+          primarySearchResults.innerHTML = '';
+          return;
+        }
+        helper.search();
+      }
+    });
+
+    searches[i].addWidget(
+      instantsearch.widgets.searchBox({
+        container: "#search-input",
+        magnifier: false,
+        reset: false,
+        queryHook: function(query, search) {
+          if (query === "") {
+            search();
+          } else {
+            search(query);
+          }
+        }
+      })
+    );
+
+    searches[i].addWidget(
+      instantsearch.widgets.hits({
+        container: i == 0 ? '#primary-search-results' : '#schema-search-results',
+        templates: {
+          empty: emptyResult,
+          item: document.getElementById(i == 0 ? 'algolia__template' : 'algolia__secondary-template').innerHTML,
+        },
+        transformData: {
+          item: function(hit) {
+            hit.raw = JSON.stringify(hit, null, 2);
+            return hit;
+          }
+        }
+      })
+    );
+
+    searches[i].start();
+  }
  
-  var search = instantsearch({
-    appId: config.applicationId,
-    apiKey: config.apiKey,
-    indexName: config.indexName,
-    urlSync: false,
-    searchFunction: function (helper) {
-      if (helper.state.query === '') {
-        primarySearchResults.innerHTML = '';
-        return;
-      }
-      helper.search();
-    }
-  });
-
-  search.addWidget(
-    instantsearch.widgets.searchBox({
-      container: "#search-input",
-      magnifier: false,
-      reset: false,
-      queryHook: function(query, search) {
-        if (query === "") {
-          search();
-        } else {
-          search(query);
-        }
-      }
-    })
-  );
-
-  search.addWidget(
-    instantsearch.widgets.hits({
-      container: '#primary-search-results',
-      templates: {
-        empty: emptyResult,
-        item: document.getElementById('algolia__template').innerHTML,
-      },
-      transformData: {
-        item: function(hit) {
-          hit.raw = JSON.stringify(hit, null, 2);
-          return hit;
-        }
-      }
-    })
-  );
-
-  var secondarySearch = instantsearch({
-    appId: config.applicationId,
-    apiKey: config.apiKey,
-    indexName: 'schema',
-    urlSync: false,
-    searchFunction: function (helper) {
-      if (helper.state.query === '') {
-        schemaSearchResults.innerHTML = '';
-        return;
-      }
-      helper.search();
-    }
-  });
-
-  secondarySearch.addWidget(
-    instantsearch.widgets.searchBox({
-      container: "#search-input",
-      magnifier: false,
-      reset: false,
-      queryHook: function(query, search) {
-        if (query === "") {
-          search();
-        } else {
-          search(query);
-        }
-      }
-    })
-  );
-
-  secondarySearch.addWidget(
-    instantsearch.widgets.hits({
-      container: '#schema-search-results',
-      templates: {
-        empty: emptyResult,
-        item: document.getElementById('algolia__secondary-template').innerHTML,
-      },
-      transformData: {
-        item: function(hit) {
-          hit.raw = JSON.stringify(hit, null, 2);
-          return hit;
-        }
-      }
-    })
-  );
-
   searchInput.addEventListener("keyup", function (event){
     if (searchInput.value == "" && !navigator.userAgent.match(/Trident.*rv:11\./)){
       modalBody.style.display = "none";
@@ -115,8 +77,5 @@ $(function(config) {
   $('#myModal').on('shown.bs.modal', function () {
     searchInput.focus();
   });
-
-  search.start();
-  secondarySearch.start();
 
 }(window.ALGOLIA_CONFIG));
