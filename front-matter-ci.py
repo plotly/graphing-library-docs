e361e44cbd7b6a3698268bc708a03e332d4288ac
Nicolas Kruchten <nicolas@plot.ly>
2021-04-12 09:25:27 -0400

    restore older frontmatter version

--- front-matter-ci.py
+++ front-matter-ci.py
@@ -91,7 +91,6 @@ else:
 # collect all posts
 meta_to_check = []
 for path in paths:
-    print("processing", str(path))
     post = frontmatter.load(str(path))
     if len(post.metadata.keys()) > 0 and "jupyter" not in post.metadata:
         meta = post.metadata
