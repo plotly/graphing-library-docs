cd20986e79df2109fab55ae3fb28f49feb43bb6b
Liam Connors <connorsl@tcd.ie>
2025-06-25 11:47:09 -0400

    Update python-sitemap.py

--- python-sitemap.py
+++ python-sitemap.py
@@ -26,7 +26,7 @@ def generate_sitemap():
     
     # Generate sitemap
     xml = '<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
-    xml += ''.join(f'  <url>\n    <loc>{url}</loc>\n  </url>\n' for url in urls)
+    xml += ''.join(f'  <url>\n    <loc>{url}</loc>\n    <priority>0.5</priority>\n  </url>\n' for url in urls)
     xml += '</urlset>'
     
     # Write to file
